7ad7ca616736b67a180d0900bb0ccbf258d3f20f
James Byron <james.byron@students.mq.edu.au>
2021-07-27 13:57:11 +1000

    Just some simple changes made to the Unity Fundamentals follow along file in class
    This commit just contains some following-along activities from today's class in the Unity Fundamentals folder. Not really much to remark on, to be honest.
    
    Starting from today I intend to begin work on the project in the main GAM405 development folder, and will likely begin populating the folder with the necessary building blocks for the project's development.

---
+++ MainGAM405Folder/GAM405_Main_Project/Unity Fundamentals/Assets/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject toggleButton;
+    public GameObject loadButton;
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadTheme();
+    }
+
+    public void SetTheme()
+    {
+        int newTheme = 1;
+
+        //Get our current theme from playerPrefs
+        if (PlayerPrefs.HasKey("Theme"))
+        {
+             newTheme = PlayerPrefs.GetInt("Theme") == 0 ? 1 : 0;
+
+           //theme = theme == 0 ? 1 : 0;
+        }
+        PlayerPrefs.SetInt("Theme", newTheme);
+        //Set it to the *other*
+        //load theme
+    }
+
+    public void StartGame()
+    {
+        //Load our game level.
+    }
+
+    public void LoadTheme()
+    {
+        //Set our menu theming on actual elements
+        toggleButton.GetComponent<Image>().color = Color.white;
+    }
+    */
+}
